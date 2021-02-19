51b4bcc171ab82e417cb176bfb9741dfb948e64f
ankitsoni301188 <57856858+ankitsoni301188@users.noreply.github.com>
2021-02-20 00:52:22 +0530

    format 1

---
+++ static/js/14.0e259061.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[14],{404:function(e,c){}}]);
+//# sourceMappingURL=14.0e259061.chunk.js.map
